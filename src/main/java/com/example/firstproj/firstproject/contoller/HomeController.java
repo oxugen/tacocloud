f675567d84e0b42fd6a8c29d3db9c5eb69e5514d
oxugen <dubrovskay.7830@mail.ru>
2023-07-25 21:18:41 +0300

    Initial project version

---
+++ src/main/java/com/example/firstproj/firstproject/contoller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.firstproj.firstproject.contoller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
+}
